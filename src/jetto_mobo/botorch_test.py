1e789ad83839c9cb9125bf9dc80009495facae0e
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2023-01-22 22:29:55 +0000

    Add more documentation

--- src/jetto_mobo/botorch_test.py
+++ src/jetto_mobo/botorch_test.py
@@ -44,8 +44,8 @@ fit_gpytorch_mll(mll)
 
 # Define the acquisition function
 # Choice of sampler:
-# Sobol is a quasirandom number generation scheme, ie generates low-discrepancy numbers
-# (on average, samples are evenly distributed to cover the space)
+# Sobol is a quasirandom number generation scheme - generates low-discrepancy sequences
+# (low-discrepancy = on average, samples are evenly distributed to cover the space)
 # BoTorch recommends using Sobol because it produces lower variance gradient estimates with much fewer samples [https://botorch.org/docs/samplers]
 sampler = SobolQMCNormalSampler(sample_shape=torch.Size([N_MC_SAMPLES]))
 qNEI = qNoisyExpectedImprovement(
@@ -61,20 +61,30 @@ bounds = torch.tensor(
 # Select next input points
 # Uses scipy.minimize(method='L-BFGS-B') to maximise the acqf
 # L-BFGS is https://epubs.siam.org/doi/abs/10.1137/0916069
-# Uses multistart:
-# - draw NUM_RESTARTS random samples from feasible region
-# - Perform local search around sampled points
-# - Take best result from all sampled points
+# Use multistart optimisation:
+# - Draw RAW_SAMPLES from the domain (uses the sampler defined in the acqf)
+# - Calculate value of acqf, a, at each RAW_SAMPLES point
+# - Weight the RAW_SAMPLES by w = exp(eta * (a - mean(a))/std(a)) where eta is some temperature parameter
+# - Draw NUM_RESTARTS from RAW_SAMPLES according to w
+# - Perform local qNEI maximisation using L-BFGS-B around NUM_RESTARTS points
+# - Take largest qNEI from all NUM_RESTARTS points
 # Does this jointly over the whole q-batch to reduce wallclock time
+# General idea: RAW_SAMPLES is cheaper than NUM_RESTARTS because no local optimisation performed
+# Performing the pre-sampling and weighting ensures that your initial points are already fairly good
 # For large q, might need to swap to sequential rather than joint optimisation
+# For explanation, see
 # https://botorch.org/v/0.1.1/docs/optimization
+# https://github.com/pytorch/botorch/issues/366#issuecomment-581951153
 candidates, _ = optimize_acqf(
     acq_function=qNEI,
     bounds=bounds,
     q=BATCH_SIZE,
-    num_restarts=NUM_RESTARTS,  # This is to do with multistart optimisation
-    raw_samples=RAW_SAMPLES,  # Number of samples for initialisation
-    options={"batch_limit": 5, "maxiter": 200},  # Passed to scipy
+    raw_samples=RAW_SAMPLES,
+    num_restarts=NUM_RESTARTS,
+    options={
+        "batch_limit": 5,
+        "maxiter": 200,
+    },  # Passed to scipy - need to check what these actually do
 )
 
 print(candidates)
