7e7c085e5f0bb31d0bac79e12138d1e9b6c90c62
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2023-02-02 09:08:48 +0000

    Delete unnecessary hdf5 functions

--- src/jetto_mobo/utils.py
+++ src/jetto_mobo/utils.py
@@ -2,7 +2,6 @@ import logging
 from datetime import datetime
 from typing import Iterable, Optional
 
-import h5py
 import numpy as np
 from matplotlib import colormaps
 
@@ -13,24 +12,6 @@ class ElapsedTimeFormatter(logging.Formatter):
             record.relativeCreated / 1000
         ).strftime("%H:%M:%S")
         return super().format(record)
-
-
-def save_to_hdf5(
-    filename: str, dataset_name: str, data: np.ndarray, overwrite: bool = False
-) -> None:
-    with h5py.File(filename, "a") as f:
-        if overwrite and dataset_name in f:
-            f[dataset_name][...] = data
-        else:
-            f.create_dataset(dataset_name, data=data)
-
-
-def load_from_hdf5(
-    filename: str,
-    dataset_name: str,
-) -> np.ndarray:
-    with h5py.File(filename, "r") as f:
-        return f[dataset_name][:]
 
 
 def pad_1d(a: Iterable[Optional[np.ndarray]], pad_value: float = np.nan) -> np.ndarray:
