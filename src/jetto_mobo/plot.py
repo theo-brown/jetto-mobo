fdedec6eef7d3db2a5e0c8cc44f1e72c0b56ea74
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2023-02-07 14:41:43 +0000

    Unify plotting

--- src/jetto_mobo/plot.py
+++ src/jetto_mobo/plot.py
@@ -1,152 +1,258 @@
+import os
+
 import h5py
 import numpy as np
+import plotly
 import plotly.graph_objects as go
 from jetto_tools.results import JettoResults
 from plotly.subplots import make_subplots
 
-from jetto_mobo.objective import scalar_cost_function
+from jetto_mobo.objective import scalar_cost_function, vector_cost_function
 from jetto_mobo.utils import rgba_colormap
 
 
-def solution_batch(group: h5py.Group):
-    figure = make_subplots(1, 2)
-    converged_cost = group["cost"][~np.isnan(group["cost"])]
-    max_cost = np.max(converged_cost)
-    min_cost = np.min(converged_cost)
-
-    for i in range(len(group["ecrh_parameters"])):
-        if np.any(np.isnan(group["cost"][i])):
-            continue
-
-        color = rgba_colormap(group["cost"][i][0], min_cost, max_cost, "viridis")
-        figure.add_traces(
-            [
-                # go.Scatter(
-                #     x=np.linspace(0, 1, len(group["target_ecrh"][i])),
-                #     # Rescale target_ecrh to match the converged value
-                #     y=group["target_ecrh"][i] * np.max(group["converged_ecrh"][i]),
-                #     name=str(i),
-                #     line_dash="dot",
-                #     line_color=color,
-                # ),
-                go.Scatter(
-                    x=np.linspace(0, 1, len(group["converged_ecrh"][i])),
-                    y=group["converged_ecrh"][i],
-                    name=str(i),
-                    line_color=color,
-                    legendgroup=str(i),
-                    hovertemplate=f"Cost: {group['cost'][i][0]:.2f}",
-                ),
-                go.Scatter(
-                    x=np.linspace(0, 1, len(group["converged_q"][i])),
-                    y=group["converged_q"][i],
-                    name=str(i),
-                    line_color=color,
-                    legendgroup=str(i),
-                    showlegend=False,
-                    hovertemplate=f"Cost: {group['cost'][i][0]:.2f}",
-                ),
-            ],
-            rows=[1, 1],
-            cols=[1, 2],
-        )
-    # Add colorbar
-    figure.add_trace(
-        go.Scatter(
-            x=[None],
-            y=[None],
-            mode="markers",
-            showlegend=False,
-            marker=dict(
-                colorscale="viridis",
-                showscale=True,
-                cmin=min_cost,
-                cmax=max_cost,
-                colorbar_title="Cost",
-                colorbar_titleside="top",
-                colorbar_outlinewidth=0,
-                colorbar_tickwidth=1,
-            ),
-            hoverinfo="none",
-        )
-    )
-    figure.update_yaxes(title="ECRH power density", row=1, col=1)
-    figure.update_yaxes(title="Safety factor", row=1, col=2)
-    figure.update_xaxes(title="Normalised radius")
-    figure.update_layout(
-        template="simple_white",
-        margin={"l": 0, "r": 0, "b": 0, "t": 0, "pad": 0},
-        legend_title="Member",
-        legend_x=1.08,
-        font_size=17,
-    )
-
-    return figure
+def get_benchmark_data(path: str):
+    benchmark = JettoResults(path=path)
+    profiles = benchmark.load_profiles()
+    timetraces = benchmark.load_timetraces()
+    return {
+        "scalar_cost": scalar_cost_function(profiles, timetraces),
+        "vector_cost": vector_cost_function(profiles, timetraces),
+        "ecrh": profiles["QECE"][-1],
+        "q": profiles["Q"][-1],
+        "xrho": profiles["XRHO"][-1],
+    }
 
 
-def optimisation_progress(hdf5_file: h5py.File):
-    figure = go.Figure()
-    optimisation_step = np.arange(len(hdf5_file["bayesopt"]) + 1)
-    bayesopt_cost = np.empty(len(hdf5_file["bayesopt"]) + 1)
+def animation(
+    input_filename: str,
+    output_filename: str,
+    title: str,
+    benchmark_path: str = "./data/benchmark",
+    cost_range=[0, 30],
+):
+    benchmark = get_benchmark_data(benchmark_path)
 
-    # Load data from benchmark
-    benchmark = JettoResults(path="./data/benchmark")
-    benchmark_cost = scalar_cost_function(
-        benchmark.load_profiles(), benchmark.load_timetraces()
-    )
+    with h5py.File(input_filename, "r") as hdf5_file:
 
-    # Load data from hdf5
-    initial_cost = np.nan_to_num(hdf5_file["initialisation/cost"], nan=1e3)
-    bayesopt_cost[0] = np.min(initial_cost)
-    for bayesopt_step in hdf5_file["bayesopt"]:
-        if "cost" in hdf5_file["bayesopt"][bayesopt_step]:
-            cost = np.nan_to_num(hdf5_file["bayesopt"][bayesopt_step]["cost"], nan=1e3)
-            bayesopt_cost[int(bayesopt_step)] = np.min(cost)
-        else:
-            bayesopt_cost[int(bayesopt_step)] = None
-    figure.add_trace(
-        go.Scatter(
-            x=optimisation_step,
-            y=bayesopt_cost,
-            name="BayesOpt",
-        )
-    )
+        def get_traces(group: str):
+            min_cost = float(np.min(np.nan_to_num(hdf5_file[group]["cost"], nan=1e3)))
+            q_traces = []
+            ecrh_traces = []
+            for i in range(batch_size):
+                # Cost and colour
+                cost_i = float(hdf5_file[group]["cost"][i])
+                if np.isnan(cost_i):
+                    continue
+                colour = rgba_colormap(
+                    cost_i,
+                    cost_range[0],
+                    cost_range[1],
+                    "viridis",
+                    alpha=1 if cost_i == min_cost else 0.3,
+                )
 
-    figure.add_trace(
-        go.Scatter(
-            x=optimisation_step,
-            y=np.ones(optimisation_step.shape) * np.min(initial_cost),
-            name="Random initialisation",
+                # Q profile
+                converged_q_i = hdf5_file[group]["converged_q"][i]
+                q_traces.append(
+                    go.Scatter(
+                        x=np.linspace(0, 1, len(converged_q_i)),
+                        y=converged_q_i,
+                        mode="lines",
+                        line_color=colour,
+                        showlegend=False,
+                    )
+                )
+
+                # ECRH profile
+                converged_ecrh_i = hdf5_file[group]["converged_ecrh"][i]
+                ecrh_traces.append(
+                    go.Scatter(
+                        x=np.linspace(0, 1, len(converged_ecrh_i)),
+                        y=converged_ecrh_i,
+                        mode="lines",
+                        line_color=colour,
+                        showlegend=False,
+                    )
+                )
+            return min_cost, q_traces, ecrh_traces
+
+        n_completed_bayesopt_steps = hdf5_file["/"].attrs["n_completed_bayesopt_steps"]
+        batch_size = hdf5_file["/"].attrs["batch_size"]
+
+        step = np.arange(n_completed_bayesopt_steps + 1)
+        cost = np.empty(len(step))
+
+        # Initialisation
+        cost[0], initial_q_traces, initial_ecrh_traces = get_traces("initialisation")
+        initial_cost_trace = go.Scatter(
+            x=step,
+            y=np.ones(len(step)) * cost[0],
             mode="lines",
             line_color="black",
             line_dash="dot",
+            name="Random initialisation",
         )
-    )
 
-    figure.add_trace(
-        go.Scatter(
-            x=optimisation_step,
-            y=np.ones(optimisation_step.shape) * benchmark_cost,
+        # Benchmark
+        benchmark_cost_trace = go.Scatter(
+            x=step,
+            y=np.ones(len(step)) * benchmark["scalar_cost"],
             name="SPR45-v9",
+            legendgroup="SPR45-v9",
             mode="lines",
-            line_color="black",
             line_dash="dash",
+            line_color="black",
         )
-    )
+        benchmark_ecrh_trace = go.Scatter(
+            x=benchmark["xrho"],
+            y=benchmark["ecrh"],
+            name="SPR45-v9",
+            legendgroup="SPR45-v9",
+            mode="lines",
+            line_dash="dash",
+            showlegend=False,
+            line_color="black",
+        )
+        benchmark_q_trace = go.Scatter(
+            x=benchmark["xrho"],
+            y=benchmark["q"],
+            name="SPR45-v9",
+            legendgroup="SPR45-v9",
+            mode="lines",
+            line_dash="dash",
+            showlegend=False,
+            line_color="black",
+        )
 
-    figure.update_layout(
-        template="simple_white",
-        margin={"l": 0, "r": 0, "b": 50, "t": 0, "pad": 0},
-        legend_orientation="h",
-        legend_yanchor="bottom",
-        legend_y=1,
-        legend_xanchor="center",
-        legend_x=0.5,
-        font_size=17,
-        xaxis_title="Optimisation step",
-        yaxis_title="Cost",
-    )
-    return figure
+        frames = []
+        for bayesopt_step in np.arange(0, n_completed_bayesopt_steps + 1):
+            figure = make_subplots(
+                rows=2, cols=2, specs=[[{"colspan": 2}, {}], [{}, {}]]
+            )
+
+            # Benchmark data
+            figure.add_traces(
+                [
+                    initial_cost_trace,
+                    benchmark_cost_trace,
+                    benchmark_ecrh_trace,
+                    benchmark_q_trace,
+                ],
+                rows=[1, 1, 2, 2],
+                cols=[1, 1, 1, 2],
+            )
+
+            if bayesopt_step == 0:
+                # Initialisation data
+                figure.add_traces(
+                    initial_ecrh_traces,
+                    rows=[2] * len(initial_ecrh_traces),
+                    cols=[1] * len(initial_ecrh_traces),
+                )
+                figure.add_traces(
+                    initial_q_traces,
+                    rows=[2] * len(initial_q_traces),
+                    cols=[2] * len(initial_q_traces),
+                )
+            else:
+                # Bayesopt data
+                cost[bayesopt_step], q_traces, ecrh_traces = get_traces(
+                    f"bayesopt/{bayesopt_step}"
+                )
+                figure.add_traces(
+                    ecrh_traces,
+                    rows=[2] * len(ecrh_traces),
+                    cols=[1] * len(ecrh_traces),
+                )
+                figure.add_traces(
+                    q_traces, rows=[2] * len(q_traces), cols=[2] * len(q_traces)
+                )
+
+            # Cost trace
+            figure.add_trace(
+                go.Scatter(
+                    x=step[: bayesopt_step + 1],
+                    y=cost[: bayesopt_step + 1],
+                    mode="lines+markers",
+                    showlegend=False,
+                    line_color=plotly.colors.qualitative.Bold[2],
+                ),
+                row=1,
+                col=1,
+            )
+
+            # Colorbar
+            figure.add_trace(
+                go.Scatter(
+                    x=[None],
+                    y=[None],
+                    mode="markers",
+                    showlegend=False,
+                    marker=dict(
+                        colorscale="viridis",
+                        showscale=True,
+                        cmin=cost_range[0],
+                        cmax=cost_range[1],
+                        colorbar_title="Cost",
+                        colorbar_titleside="top",
+                        colorbar_outlinewidth=0,
+                        colorbar_tickwidth=1,
+                        colorbar_len=0.5,
+                        colorbar_yanchor="bottom",
+                        colorbar_y=0,
+                        colorbar_xanchor="left",
+                        colorbar_x=1.02,
+                    ),
+                    hoverinfo="none",
+                )
+            )
+
+            figure.update_xaxes(
+                title_text="Optimisation step",
+                row=1,
+                col=1,
+                range=[0, n_completed_bayesopt_steps],
+            )
+            figure.update_yaxes(title_text="Minimum cost", row=1, col=1)
+            figure.update_xaxes(
+                title_text="Normalised radius", row=2, col=1, range=[0, 1]
+            )
+            figure.update_yaxes(
+                title_text="ECRH power density",
+                row=2,
+                col=1,
+                range=[0, 1.25 * np.max(benchmark["ecrh"])],
+            )
+            figure.update_xaxes(
+                title_text="Normalised radius", row=2, col=2, range=[0, 1]
+            )
+            figure.update_yaxes(
+                title_text="Safety factor",
+                row=2,
+                col=2,
+                range=[0, 1.1 * np.max(benchmark["q"])],
+            )
+            figure.update_layout(
+                template="simple_white",
+                margin={"l": 0, "r": 0, "b": 0, "t": 0, "pad": 0},
+                font_size=20,
+                legend_orientation="h",
+                legend_yanchor="bottom",
+                legend_y=1,
+                legend_xanchor="right",
+                legend_x=1,
+                title_text=title,
+                title_x=0.5,
+            )
+
+            frames.append(figure)
+
+    os.makedirs(output_filename)
+    for i, frame in enumerate(frames):
+        frame.write_image(
+            f"{output_filename}/{i:02}.png", width=1920, height=1080, scale=1
+        )
 
 
 if __name__ == "__main__":
@@ -154,21 +260,17 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "mode",
+        "input_file",
         type=str,
-        choices=["solution", "progress"],
     )
     parser.add_argument(
-        "input_file",
+        "title",
+        type=str,
+    )
+    parser.add_argument(
+        "output_file",
         type=str,
     )
     args = parser.parse_args()
 
-    with h5py.File(args.input_file, "r") as f:
-        if args.mode == "solution":
-            solution_batch(f["initialisation"]).show()
-            if "bayesopt" in f.keys():
-                for i in range(len(f["bayesopt"])):
-                    solution_batch(f[f"bayesopt/{i+1}"]).show()
-        elif args.mode == "progress":
-            optimisation_progress(f).show()
+    animation(args.input_file, args.output_file, args.title)
