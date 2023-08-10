efb314ebc75f4fb08017b3bc2e167d643142d7a3
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2023-08-10 10:34:17 +0100

    Add poster

--- docs/source/publications.rst
+++ docs/source/publications.rst
@@ -3,7 +3,7 @@ Publications
 
 We're currently working on a paper to be submitted to the IEEE Transactions on Plasma Science (TPS).
 
-Until then, you can cite our :download:`poster <files/Multi-objective Bayesian optimisation for design of Pareto-optimal current drive profiles in STEP - poster.pdf>` from the 2023 IEEE Symposium on Fusion Engineering (SOFE):
+Until then, you can cite our :download:`poster <../../publications/poster/Multi-objective Bayesian optimisation for design of Pareto-optimal current drive profiles in STEP - poster.pdf>` from the 2023 IEEE Symposium on Fusion Engineering (SOFE):
 
     Brown, T., Marsden, S., Casson, F., Terenin, A., and Gopakumar, V., "Multi-objective Bayesian optimisation for design of Pareto-optimal current drive profiles in STEP", presented at 30th IEEE Symposium on Fusion Engineering, Oxford, UK, 12th July 2023.
 
---
+++ publications/poster/scripts/mobo_new.py
@@ -0,0 +1,189 @@
+import h5py
+import numpy as np
+import plotly
+import plotly.io as pio
+import utils
+from netCDF4 import Dataset
+from plotly import graph_objects as go
+from plotly.subplots import make_subplots
+
+from jetto_mobo.old.objective import vector_objective
+from jetto_mobo.old.utils import get_pareto_dominant_mask
+
+# disable mathjax
+pio.kaleido.scope.mathjax = None
+
+# Load data from MOBO
+n_steps = 12
+batch_size = 30
+n_objectives = 7
+n_parameters = 11
+n_radial_points = 150
+
+parameters = np.zeros((n_steps * batch_size, n_parameters))
+objective_values = np.zeros((n_steps * batch_size, n_objectives))
+converged_ecrh = np.zeros((n_steps * batch_size, 150))
+converged_q = np.zeros((n_steps * batch_size, 150))
+
+with h5py.File("../../data/mobo/joint/sog_fm_cpu/bayesopt.hdf5") as hdf5:
+    for i in np.arange(n_steps):
+        for j in np.arange(batch_size):
+            if not np.any(np.isnan(hdf5[f"bayesopt/{i+1}/value"][j])):
+                objective_values[i * batch_size + j] = hdf5[f"bayesopt/{i+1}/value"][j]
+                parameters[i * batch_size + j] = hdf5[
+                    f"bayesopt/{i+1}/ecrh_parameters"
+                ][j]
+                converged_ecrh[i * batch_size + j] = hdf5[
+                    f"bayesopt/{i+1}/converged_ecrh"
+                ][j]
+                converged_q[i * batch_size + j] = hdf5[f"bayesopt/{i+1}/converged_q"][j]
+
+# Get Pareto optimal solutions
+pareto_dominant_mask = get_pareto_dominant_mask(objective_values, allow_zero=False)
+
+# Get solutions where qmin > 2
+qmin_mask = np.min(converged_q, axis=1) >= 2
+
+# Get solutions where q0 < 5
+q0_mask = converged_q[:, 0] <= 5
+
+# Generate figure
+figure = make_subplots(
+    rows=2,
+    cols=2,
+    specs=[[{"rowspan": 2, "type": "polar", "b": 0.1}, {}], [{}, {}]],
+    horizontal_spacing=0.13,
+    vertical_spacing=0.12,
+)
+
+mask = pareto_dominant_mask & qmin_mask & q0_mask
+
+
+def add_solution_to_figure(
+    objective_value, ecrh, q, name, colour, dash=False, fill_alpha=0.1
+):
+    figure.add_traces(
+        [
+            go.Scatterpolar(
+                r=np.concatenate([objective_value, [objective_value[0]]]),
+                theta=[
+                    "Proximity<br>of q<sub>0</sub> to q<sub>min</sub>",
+                    "Proximity of<br>q<sub>min</sub> to 2.2",
+                    "Proximity of<br>q<sub>min</sub> to centre",
+                    "Monotonicity<br>of q",
+                    "Monotonicity<br>of ∇q",
+                    "Location<br>of q = 3",
+                    "Location<br>of q = 4",
+                    "Proximity<br>of q<sub>0</sub> to q<sub>min</sub>",
+                ],
+                legendgroup=name,
+                name=name,
+                showlegend=False,
+                # fill="toself",
+                # fillcolor=utils.colour_to_rgba(colour, fill_alpha),
+                # line_dash="dash" if dash else None,
+                line_color=colour,
+                line_width=3,
+                marker_size=10,
+            ),
+            go.Scatter(
+                x=np.linspace(0, 1, n_radial_points),
+                y=ecrh,
+                legendgroup=name,
+                name=name,
+                showlegend=False,
+                mode="lines",
+                line_dash="dash" if dash else None,
+                line_color=colour,
+                line_width=3,
+            ),
+            go.Scatter(
+                x=np.linspace(0, 1, n_radial_points),
+                y=q,
+                legendgroup=name,
+                name=name,
+                showlegend=True,
+                mode="lines",
+                line_dash="dash" if dash else None,
+                line_color=colour,
+                line_width=3,
+            ),
+            go.Scatter(
+                x=[np.linspace(0, 1, n_radial_points)[np.argmin(q)]],
+                y=[np.min(q)],
+                legendgroup=name,
+                name=f"{name} - minimum",
+                mode="markers",
+                line_color=colour,
+                showlegend=False,
+                marker_size=10,
+            ),
+        ],
+        rows=[1, 1, 2, 2],
+        cols=[1, 2, 2, 2],
+    )
+
+
+# Plot Bayesopt results
+solution_0 = np.nonzero(mask)[0][0]
+solution_2 = np.nonzero(mask)[0][2]
+solution_4 = np.nonzero(mask)[0][4]
+add_solution_to_figure(
+    objective_values[solution_0],
+    converged_ecrh[solution_0],
+    converged_q[solution_0],
+    name=f"Solution 1/8",
+    colour=utils.colours[0],
+)
+add_solution_to_figure(
+    objective_values[solution_2],
+    converged_ecrh[solution_2],
+    converged_q[solution_2],
+    name=f"Solution 3/8",
+    colour=utils.colours[1],
+)
+add_solution_to_figure(
+    objective_values[solution_4],
+    converged_ecrh[solution_4],
+    converged_q[solution_4],
+    name=f"Solution 5/8",
+    colour=utils.colours[2],
+)
+
+# Layout settings
+figure.update_layout(
+    template="simple_white",
+)
+figure.update_layout(
+    polar_radialaxis_range=[0, 1.01],
+    polar_radialaxis_showgrid=True,
+    polar_radialaxis_gridcolor="grey",
+    polar_angularaxis_showgrid=True,
+    polar_angularaxis_showline=False,
+    polar_angularaxis_gridcolor="grey",
+    legend_orientation="h",
+    legend_x=1,
+    legend_y=1,
+    legend_xanchor="right",
+    legend_yanchor="bottom",
+    margin={"l": 110, "r": 20, "b": 20, "t": 20, "pad": 0},
+    font_size=22,
+)
+figure.add_annotation(
+    text="Vector objective values",
+    xref="paper",
+    yref="paper",
+    x=0.15,
+    y=-0.17,
+    showarrow=False,
+    font_size=26,
+)
+figure.update_yaxes(title="QECE [W]", row=1, col=2, linewidth=3)
+figure.update_yaxes(range=[1.8, None], title="Safety factor", row=2, col=2, linewidth=3)
+figure.update_xaxes(title="Normalised radius", row=2, col=2, range=[0, 1], linewidth=3)
+figure.update_xaxes(row=1, col=2, range=[0, 1], linewidth=3)
+figure.write_image(
+    "../images/mobo_new.svg",
+    width=1280,
+    height=600,
+)
